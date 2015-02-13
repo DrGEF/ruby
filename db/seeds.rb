3bf9e3f4e4a8980ea24f8c9081f3702b584191b8
drgef <thedrgef@gmail.com>
2015-02-13 21:43:53 +0300

    db seed edit. remove idea

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,28 @@
 Product.delete_all
+Product.create(
+    title: 'Заголовок',
+    description:
+    %{<p>
+        Описание товара... Длинное длинное...
+    </p>},
+    image_url: 'ruby.jpg',
+    price: 99.99
+)
 Product.create!(
-           title: 'Заголовок',
-           description:
-           %{<p>
-                Описание товара... Длинное длинное...
-          </p>},
-           image_url: 'ruby.jpg',
-           price: 99.99
+    title: 'Другой заголовок',
+    description:
+    %{<p>
+        Описание товара... Длинное длинное...
+    </p>},
+    image_url: 'ruby.jpg',
+    price: 99.99
+)
+Product.create!(
+    title: 'Совсем другой заголовок',
+    description:
+    %{<p>
+        Описание товара... Длинное длинное...
+    </p>},
+    image_url: 'ruby.jpg',
+    price: 99.99
 )
