6e67556ea97f19c1ff7511fb37a215e87d9410bc
drgef <thedrgef@gmail.com>
2015-06-22 22:32:59 +0300

    last admin ready

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,13 +7,15 @@ class SessionsController < ApplicationController
   def create
     if User.count == 0
       user = User.create(name: params[:name], password: params[:password])
-      redir user.id
+      session[:user_id] = user.id
+      redirect_to admin_url
     else
     	user = User.find_by(name: params[:name])
     	if user and user.authenticate(params[:password])
-    		redir user.id
+    		session[:user_id] = user.id
+        redirect_to admin_url
     	else
-    		redirect_to login_url, alert: "неверная комбинация имени и пароля"
+    		redirect_to login_url, alert: "Неверное сочетание логин/пароль"
     	end
     end
   end
@@ -21,9 +23,5 @@ class SessionsController < ApplicationController
   def destroy
   	session[:user_id] = nil
   	redirect_to store_url, notice: "Сеанс работы завершен"
-  end
-  def redir user_id
-    session[:user_id] = @user_id
-    redirect_to admin_url
   end
 end
